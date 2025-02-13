c8d3a7f9bafbaf1f96afd6ecad9ef61a20a79a5c
Ivan Busenko <132225898+Busenko@users.noreply.github.com>
2025-02-13 11:02:41 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginButton = document.querySelector(".header__login");
+    const popupLogin = document.querySelector(".header__popup-login");
+    const closePopup = document.createElement("span");
+    
+    closePopup.innerHTML = "&times;";
+    closePopup.classList.add("popup-close");
+    popupLogin.prepend(closePopup);
+    
+    loginButton.addEventListener("click", function () {
+        popupLogin.classList.toggle("active");
+    });
+    
+    closePopup.addEventListener("click", function () {
+        popupLogin.classList.remove("active");
+    });
+    
+    document.addEventListener("click", function (e) {
+        if (!popupLogin.contains(e.target) && !loginButton.contains(e.target)) {
+            popupLogin.classList.remove("active");
+        }
+    });
+});
